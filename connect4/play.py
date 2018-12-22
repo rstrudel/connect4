f9eac23edefe329973dde134c5d59a14aabbf5dd
Robin Strudel <rstrudel@gmail.com>
2018-12-22 23:07:17 +0100

    Clean play

--- connect4/play.py
+++ connect4/play.py
@@ -10,6 +10,7 @@ from .node import Node
 
 np.random.seed(0)
 
+
 def explore(mcts, max_time, node=None, state=None):
     count = 0
     t0 = time.time()
@@ -20,6 +21,7 @@ def explore(mcts, max_time, node=None, state=None):
             mcts.sim()
         count += 1
     # print('Played {} games'.format(count))
+
 
 # initialize game and mcts
 game_cls = Connect4
@@ -37,12 +39,12 @@ while game.is_won() == 0 and len(valid_actions) > 0:
     print('Board:\n', game.to_str())
     action = None
     while action not in valid_actions:
-        action = int(input('Action:'))
+        action = int(input('action: '))
     game.play(action)
     node_action = node.get_child_action(action)
     if node_action is None:
         node_action = Node(game.board, game.is_won()*player,
-                        action, node)
+                           action, node)
         node.add_child(node_action)
     node = node_action
 
@@ -50,7 +52,7 @@ while game.is_won() == 0 and len(valid_actions) > 0:
     explore(mcts, 1, node=node, state=game.board)
     node, action, scores = node.select_action()
     game.play(action)
-    print('AI played action {}'.format(action))
+    print('ai action: {}'.format(action))
     print('Scores', scores, '\n')
     valid_actions = game.valid_actions(game.board)
 
